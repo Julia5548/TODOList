62f279ef8cade5ea03b2b7ebd30f90bbeeaea260
Julia <negodyaeva.yulya@mail.ru>
2020-08-06 15:35:48 +0800

    добавлен Router

--- src/ pages/User.tsx
+++ src/ pages/User.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import  WindowTask  from '../components/MainWindowTodo';
+import  Authorization  from '../components/Authorization';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import  Registration  from '../components/Registration';
+import  ResetPassword  from '../components/ResetPassword';
+import  MainWindowTodo  from '../components/MainWindowTodo';
+
+type DataUser = {
+    name? : string;
+    email : string;
+    password:string;
+    code?: string;
+}
 
 export const User = (props: any) => {
 
-    const handleSubmit = (values: any) => {
+    const handleSubmit = (values: DataUser) => {
         window.alert(JSON.stringify(values));
     }
 
     return (
-        <WindowTask onSubmit={handleSubmit} {...props} />
+        <BrowserRouter>
+            <Switch>
+                <Route path = "/" exact render ={() => <Authorization onSubmit = {handleSubmit} {...props} /> } />
+                <Route path = "/registration"  render ={() => <Registration onSubmit = {handleSubmit} {...props} /> } />
+                <Route path = "/reset"  render ={() => <ResetPassword onSubmit = {handleSubmit} {...props} /> } />
+                <Route path = "/todo"  render ={() => <MainWindowTodo onSubmit = {handleSubmit} {...props} /> } />
+            </Switch>
+        </BrowserRouter>
     );
 }
 
